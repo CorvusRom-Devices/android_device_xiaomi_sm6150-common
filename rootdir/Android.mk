445e400a589a0d0157dc11d6f9c292785d5d2783
rakesh roshan <rakeshraimca@gmail.com>
2020-08-25 11:58:35 +0530

    Fixing declaration issues

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,8 +19,6 @@ LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-include $(BUILD_PREBUILT)
 
 # Spectrum scripts
 
